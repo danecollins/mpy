1da33810a69dde7d09ee7e47456a8578f6000cb6
Dane Collins <dane@pobox.com>
2013-07-17 16:22:08 -0700

    fixed the copying of files.  Need to add a file list.

--- push_files/push.py
+++ push_files/push.py
@@ -1,22 +1,26 @@
 import os, time
+import shutil
 
 ## TODO: figure out the right way to handle
 ## file paths that work on osx and windows
 
-files = ["time_test.txt"]
+files = ["time_test.txt" , "fubar.txt"]
 source_dir = '.\\'
-dest_dir = '\\\\ts1a\\dane\\revrec\\'
+dest_dir = '\\\\ts1a\\dane\\push_files\\'
 
 for file in files:
 	source_path = source_dir + file
 	dest_path   = dest_dir   + file
 	print "comparing ", source_path, "with", dest_path
-	#
-	# source_time = os.path.getmtime(source_path)
-	# dest_time   = os.path.getmtime(dest_path)
+
+	source_time = os.path.getmtime(source_path)
+	try:
+		dest_time   = os.path.getmtime(dest_path)
+	except WindowsError:
+		dest_time   = 0
 	
-	# if (source_time > dest_time):
-	# 	os.path.copy(source_path,dest_path)
-	# 	print "copied ", file
+	if (source_time > dest_time):
+	 	shutil.copyfile(source_path,dest_path)
+	 	print "copied ", file
 
 
