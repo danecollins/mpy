952c5da3a905242582e76bac4577fb157fbb8e68
Dane Collins <dane@pobox.com>
2014-03-10 10:50:19 -0700

    fixed account number used in splitting of allocation lines which had a transposition

--- ni_import_US/run_tests.bat
+++ ni_import_US/run_tests.bat
@@ -4,10 +4,10 @@ if exist output.csv del output.csv
 
 c:\python27\python.exe readinput.py testinput0.txt
 echo ---------------------------------------------
-fc output.csv testoutput0.csv
+fc /l /lb1 output.csv testoutput0.csv
 
 if exist output.csv del output.csv
 
 c:\python27\python.exe readinput.py testinput1.txt
 echo ---------------------------------------------
-fc output.csv testoutput1.csv
+fc /l /lb1 output.csv testoutput1.csv
