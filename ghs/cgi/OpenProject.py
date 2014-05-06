2baafc5687dc6c0d24c98933c331d46404f70465
Dane Collins <dane@pobox.com>
2014-05-06 14:26:39 -0700

    added windows switch to bin path and fixed typo

--- ghs/cgi/OpenProject.py
+++ ghs/cgi/OpenProject.py
@@ -2,15 +2,23 @@
 import cgi
 import os
 
-bin_path='/Users/dane/work_scripts/ghs/bin/'
+if os.name == 'nt':
+	bin_path = '/src/work_scripts/ghs/bin/'
+else:
+	bin_path='/Users/dane/work_scripts/ghs/bin/'
+
 
 def main():
 	print "Content-type: text/html\n"
+	print "In OpenProject.py"
 	form = cgi.FieldStorage()
 	if form.has_key("name") and form["name"].value != "":
 		print "<h1>I will open project named:", form["name"].value,"</h1>"
 
-		os.system(bin_path + 'open_project.py ' + form["name"].value)
+		command = bin_path + 'open_project.py ' + form["name"].value
+		print "executing command: " + command
+
+		os.system(command)
 	else:
 		print "<h1> Error! link has no project name.</h1>"
 
