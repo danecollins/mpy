ccb9bc5f4c656c8976a0079a241b26c46cfb99eb
Dane Collins <dane@pobox.com>
2014-05-13 16:57:58 -0700

    Added retrieve of remote project files

--- ghs/cgi/OpenProject.py
+++ ghs/cgi/OpenProject.py
@@ -1,25 +1,53 @@
 #!/usr/bin/python
 import cgi
 import os
+import sys
+import urllib
 
-if os.name == 'nt':
-	bin_path = '/src/work_scripts/ghs/bin/'
-else:
-	bin_path='/Users/dane/work_scripts/ghs/bin/'
 
+def get_parameter(name):
+	if sys.argv[1]:
+		# form being executed from command line for testing
+		return sys.argv[1]
+	else:
+		# form being executed from web server
+		form = cgi-FieldStorage()
+		if (form.has_key(name) and form[name].value != ""):
+			return(form[name].value)
+		else:
+			return False
+
+def get_project(project_url):
+	if (http in project_url):
+		# web file request, download file
+		(filename,headers) = urllib.urlretrieve(project_url)
+	else:
+		# local file request
+		filename = project_url
+	return(filename)
 
 def main():
 	print "Content-type: text/html\n"
-	print "In OpenProject.py"
-	form = cgi.FieldStorage()
-	if form.has_key("name") and form["name"].value != "":
-		print "<h1>I will open project named:", form["name"].value,"</h1>"
+	print "<head><title>In OpenProject.py</title></head>"
+	print "<body>"
+	print "<h1>Command debug log</h1>"
+	project_name = get_parameter("name")
+	if project_name:
+		local_name = get_project(project_name)
+		print "<p>Opening project: %s</p>" % project_name
 
-		command = bin_path + 'open_project.py ' + form["name"].value
-		print "executing command: " + command
-
-		os.system(command)
+		if os.name == 'nt':
+			print "<p>On Windows, executing awrde.Open(%s)</p>" % project_name
+			import win32com.client
+			awrde=win32com.client.Dispatch("MWOApp.MWOffice")
+			awrde.Open(project_name)
+		else:
+			print "<p>On OSX so not executing awrde.Open(%s)</p>" % project_name
 	else:
-		print "<h1> Error! link has no project name.</h1>"
+		print "<h2> Error! link has no project name.</h2>"
+	print "</body>"
 
 main()
+
+
+	
