6b0c5b108d70bd205a2bcee191c3598bbbb67bdd
Dane Collins <dane@pobox.com>
2014-12-08 21:12:30 -0800

    Make osx/win compatible

--- push_files/publish.py
+++ push_files/publish.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 import os, time
 import shutil
 import sys
+import re
 
 #####################################################################
 ##
@@ -26,82 +27,103 @@ import sys
 ##         
 #####################################################################
 
-did_something = False
-if '-s' in sys.argv:
-	print('=== Running in simulation mode, will not copy files. ===')
-	execute = False
-else:
-	execute = True
 
-verbose_mode = True if '-v' in sys.argv else False
-
-if not os.path.exists('Publish.txt'):
-	print('No Publish.txt, nothing to do.')
-	exit(0)
-
-try:
-	with open('Publish.txt') as fp:
-		files = list()
-		cp = namedtuple('CopyPairs',['frompath','topath'])
-		for line in fp:
-			line = line.strip()
-			if line != '':
-				(frompath,topath)= line.split('->')
-				frompath = frompath.strip()
-				topath   = topath.strip()
-				if os.path.exists(topath):
-					if os.path.isdir(topath):
-						# destination is a directory so create filename
-						topath = topath + '\\' + frompath
-					files.append(cp._make([frompath,topath]))
-				else:
-					print('Destination directory %s does not exist! Skipping...' % topath)
-except:
-	print('Error parsing Publish.txt file')
-	print("Unexpected error:", sys.exc_info()[0])
-	exit(1)
-
-if verbose_mode:
-	for x in files:
-		print('Work to be done:')
-		print('   From: ',x.frompath)
-		print('   To:   ',x.topath)
-		print()
-
-
-for file in files:
-
-	if not os.path.exists(file.frompath):
-		print('Source file: %s does not exist! Skipping...' % file.frompath)
-		continue
+def get_file_list(input_file='Publish.txt'):
+	if not os.path.exists(input_file):
+		print('No Publish.txt, nothing to do.')
+		exit(0)
 
 	try:
-		source_time = os.path.getmtime(file.frompath)
-	except:
-		print('Could not get the modified time of: %s!' % file.frompath)
-		print('Error is:', sys.exc_info()[0])
-		print('Exiting...')
-		exit(1)
+		with open(input_file) as fp:
+			files = list()
+			cp = namedtuple('CopyPairs',['frompath','topath','tofile'])
+			for line in fp:
+				line = line.strip()
+				if line != '':
+					(frompath,topath)= re.split(r'\s*->\s*', line)
+					### topath can be a directory or a full filename
+					### determine which by seeing if the last component
+					### has a dot in it which makes it a file
+					###
+					### NOTE: if file has no . this won't work!
+					###
+					pathelements = re.split(r'[/\\]',topath)
+					if '.' not in pathelements[-1]:
+						pathelements.append(frompath)
 
-	if os.path.exists(file.topath):
+					topath = os.sep.join(pathelements[:-1])
+					tofile = os.sep.join(pathelements)
+
+					files.append(cp._make([frompath,topath,tofile]))
+	except:
+		print('Error parsing Publish.txt file')
+		print("Unexpected error:", sys.exc_info()[0])
+		return(None)
+	return(files)
+
+
+def copy_files(files,execute,verbose_mode):
+	global did_something
+
+	if verbose_mode:
+		for x in files:
+			print('Work to be done:')
+			print('   From: ',x.frompath)
+			print('   To:   ',x.topath)
+			print()
+
+
+	for file in files:
+
+		if not os.path.exists(file.frompath):
+			print('Source file: %s does not exist! Skipping...' % file.frompath)
+			continue
+
 		try:
-			dest_time   = os.path.getmtime(file.topath)
+			source_time = os.path.getmtime(file.frompath)
 		except:
 			print('Could not get the modified time of: %s!' % file.frompath)
 			print('Error is:', sys.exc_info()[0])
 			print('Exiting...')
 			exit(1)
+
+		if os.path.isdir(file.topath):
+			print('Destination directory %s does not exist!' % file.topath)
+			print('Exiting...')
+			exit(1)
+
+		if os.path.exists(file.tofile):
+			try:
+				dest_time   = os.path.getmtime(file.tofile)
+			except:
+				print('Could not get the modified time of: %s!' % file.tofile)
+				print('Error is:', sys.exc_info()[0])
+				print('Exiting...')
+				exit(1)
+		else:
+			dest_time = 0
+
+		if (source_time > dest_time):
+			did_something = True
+			if execute:
+				shutil.copyfile(file.frompath,file.tofile)
+			print('copying %s to %s' % (file.frompath,file.tofile))
+
+if __name__ == '__main__':
+	did_something = False
+	if '-s' in sys.argv:
+		print('=== Running in simulation mode, will not copy files. ===')
+		execute = False
 	else:
-		dest_time = 0
+		execute = True
 
-	if (source_time > dest_time):
-		did_something = True
-		if execute:
-			shutil.copyfile(file.frompath,file.topath)
-		print('copying %s to %s' % (file.frompath,file.topath))
+	verbose_mode = True if '-v' in sys.argv else False
 
-if not did_something:
-	print('Nothing to be done.')
+	files = get_file_list()
+	copy_files(files,execute,verbose_mode)
+
+	if not did_something:
+		print('Nothing to be done.')
 
 
 
---
+++ push_files/test_publish.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from collections import defaultdict,Counter
+
+import publish
+import unittest
+import os
+
+
+class TestPublish(unittest.TestCase):
+    def test_get_file_list1(self):
+        files = publish.get_file_list('list1.txt')
+        assert files[0].frompath == 'publish.txt'
+        assert files[0].topath.startswith('/users/dane/ignore_path_check')
+
+    def test_get_file_list2(self):
+        files = publish.get_file_list('list2.txt')
+        assert files[0].frompath == 'publish.txt'
+        assert files[0].topath.startswith('/users/dane')
+
+    def test_get_file_list3(self):
+        files = publish.get_file_list('list3.txt')
+        assert files[0].frompath == 'publish.txt'
+        assert files[0].topath.startswith('/users/dcollins/ignore_path_check')
+
+    def test_get_file_list4(self):
+        files = publish.get_file_list('list4.txt')
+        assert files[0].frompath == 'publish.txt'
+        assert files[0].tofile.startswith('/users/dane/ignore_path_check/foo.txt')
+
+unittest.main()
